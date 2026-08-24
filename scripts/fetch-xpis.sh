f5ba583b80bc2b6d7dcededad64aa2eff274ecde
asystech-chen <augustwkr@outlook.com>
2026-08-24 23:45:21 +0800

    chore: fetch-xpis.sh 清理已弃用扩展下载 URL（Absolute Enable/SessionsRestore/easyscreenshot）

--- scripts/fetch-xpis.sh
+++ scripts/fetch-xpis.sh
@@ -10,9 +10,6 @@ mkdir -p "$XPI_DIR"
 # Extension download URLs (must be direct XPI links)
 declare -A XPI_URLS
 XPI_URLS["idcac-pub@guus.ninja"]="https://addons.mozilla.org/firefox/downloads/file/4637154/istilldontcareaboutcookies-1.1.9.xpi"
-XPI_URLS["{9350bc42-47fb-4598-ae0f-825e3dd9ceba}"]="https://addons.mozilla.org/firefox/downloads/file/4274207/absolute_enable_right_click-1.3.9resigned1.xpi"
-XPI_URLS["{b184d107-461b-4cfe-b4ba-771406e90c48}"]="https://addons.mozilla.org/firefox/downloads/file/4274183/912887-1.1resigned1.xpi"
-XPI_URLS["easyscreenshot@mozillaonline.com"]="https://addons.mozilla.org/firefox/downloads/file/4049242/easyscreenshot-3.109.xpi"
 
 for id in "${!XPI_URLS[@]}"; do
     url="${XPI_URLS[$id]}"
